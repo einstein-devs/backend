d545c8ac8224ef7b9e2b56cbbc85d251047c993e
fernando-bertolo <fernando.bertolo.jr123@gmail.com>
2023-04-25 20:50:33 -0300

    Alterações propostas realizadas

--- src/modules/evento/event.controller.ts
+++ src/modules/evento/event.controller.ts
@@ -1,23 +1,34 @@
-import { Controller, Post, Get, Patch, Delete, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
 import { EventService } from './event.service';
 import { ParamsGetEvento } from './dto/params-post-eventos';
 import { eventDTO } from './dto/create-evento-dto';
 import { stringify } from 'querystring';
 import { ApiBody } from '@nestjs/swagger';
+import { DefaultResponseDTO } from 'src/shared/dto/default-response.dto';
 
 @Controller('/eventos')
 export class EventController {
-    constructor (private readonly eventService: EventService){}
+  constructor(private readonly eventService: EventService) {}
 
-    // Criação de eventos
-    @Post('/create')
-    async postEvent(@Body() createEventDto: eventDTO) {
-        return this.eventService.createEvent(createEventDto);
-    }
-    
-    //Listagem de eventos
-    @Get('/consulta')
-    async findMany() {
-        return await this.eventService.findMany();
-    }
+  // Criação de eventos
+  @Post('/create')
+  async postEvent(@Body() createEventDto: eventDTO) {
+    const eventCreate = await this.eventService.createEvent(createEventDto);
+    return new DefaultResponseDTO(eventCreate, 'Evento criado com sucesso');
+  }
+
+  //Listagem de eventos
+  @Get('/consulta')
+  async findMany() {
+    const events = await this.eventService.findMany();
+    return new DefaultResponseDTO(events, 'Eventos retornados com sucesso');
+  }
 }
--- src/modules/evento/event.service.ts
+++ src/modules/evento/event.service.ts
@@ -4,28 +4,32 @@ import { eventDTO } from './dto/create-evento-dto';
 
 @Injectable()
 export class EventService {
-    constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
-    //Criação de eventos
-    async createEvent(data: eventDTO) {
-        try{
-            const eventCreate = await this.prismaService.evento.create({
-                data: {
-                    titulo: data.titulo,
-                    codigo: data.codigo,
-                    descricao: data.descricao,
-                    dataHoraInicio: data.dataHoraInicio,
-                    dataHoraTermino: data.dataHoraTermino,
-                }
-            });
-            return eventCreate;
-        } catch{
-            throw new BadRequestException('Ocorreu um erro ao criar o evento');
-        }
+  //Criação de eventos
+  async createEvent(data: eventDTO) {
+    try {
+      const eventCreate = await this.prismaService.evento.create({
+        data: {
+          titulo: data.titulo,
+          codigo: data.codigo,
+          descricao: data.descricao,
+          dataHoraInicio: data.dataHoraInicio,
+          dataHoraTermino: data.dataHoraTermino,
+        },
+      });
+      return eventCreate;
+    } catch {
+      throw new BadRequestException('Ocorreu um erro ao criar o evento');
     }
+  }
 
-    //Listagem de eventos
-    async findMany() {
-        return await this.prismaService.evento.findMany();
-      }
+  //Listagem de eventos
+  async findMany() {
+    try {
+      return await this.prismaService.evento.findMany();
+    } catch {
+      throw new BadRequestException('Ocorreu um erro ao listar os eventos');
+    }
+  }
 }
