321c907afabff7577566fc7c8f50ff14f31b0add
Joao Mateus <mateusgamescoelho547@gmail.com>
2023-05-21 23:16:35 -0300

    main

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
 import {
-  ApiBody,
-  ApiOkResponse,
-  ApiTags,
-  ApiUnauthorizedResponse,
+    ApiBody,
+    ApiOkResponse,
+    ApiTags,
+    ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 import { DefaultResponseDTO } from 'src/shared/dto/default-response.dto';
 import { AuthService } from './auth.service';
@@ -12,28 +12,30 @@ import { LocalAuthGuard } from './guards/local-auth.guard';
 @ApiTags('AuthController')
 @Controller('/auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+    constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
-  @Post()
-  @ApiBody({
-    required: true,
-    schema: {
-      default: {
-        codigo: 'codigo_aqui',
-        senha: 'senha_aqui',
-      },
-    },
-  })
-  @ApiOkResponse({
-    description: 'Quando login valido, retornara token de usuario',
-  })
-  @ApiUnauthorizedResponse({
-    description:
-      'Quando login invalido, retorna a mensagem: Código ou senha inválidos',
-  })
-  async loginUsuario(@Req() req, @Res() res,): Promise<DefaultResponseDTO<string>> {
-    const token = await this.authService.login(req.user);
-    return res.status(200).json(new DefaultResponseDTO<string>(token));
-  }
+    @UseGuards(LocalAuthGuard)
+    @Post()
+    @ApiBody({
+        required: true,
+        schema: {
+            default: {
+                codigo: 'codigo_aqui',
+                senha: 'senha_aqui',
+            },
+        },
+    })
+    @ApiOkResponse({
+        description: 'Quando login valido, retornara token de usuario',
+    })
+    @ApiUnauthorizedResponse({
+        description:
+            'Quando login invalido, retorna a mensagem: Código ou senha inválidos',
+    })
+    async loginUsuario(@Req() req, @Res() res) {
+        const token = await this.authService.login(req.user);
+        return res
+            .status(200)
+            .json(new DefaultResponseDTO({ user: req.user, token }));
+    }
 }
